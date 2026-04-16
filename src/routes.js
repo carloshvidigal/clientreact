6adaca63558f1aa15393702db7721d0f6e9ca2d4
chmov <chmov@PC-CARLOSHMV>
2026-04-16 14:33:40 -0300

    Atualiza Routes

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
+import Alunos from './pages/Alunos';
+import NovoAluno from './pages/NovoAluno';
+
+
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login />} />
-                {/* 
+                <Route path="/" element={<Login />} />                
                 <Route path="/alunos" element={<Alunos />} />
-                <Route path="/aluno/novo/:alunoId" element={<NovoAluno />} />
-                */}
+                <Route path="/aluno/novo/:alunoid" element={<NovoAluno />} />               
             </Routes>
         </BrowserRouter>
     );
