915fa797ad38a06afb86b630db4637728afe7219
chmov <chmov@PC-CARLOSHMV>
2026-04-17 17:41:06 -0300

    feat: condição para id aluno para renderizar página de inclusão ou edição

--- src/pages/NovoAluno/index.js
+++ src/pages/NovoAluno/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './styles.css';
-import {Link} from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import {FiCornerDownLeft, FiUserPlus } from 'react-icons/fi';
 
 
+
 export default function NovoAluno(){ 
+
+    const {alunoId} = useParams();
+
+    
     return(
         <div className ="novo-aluno-container">
             <div className="content">
                 
                 <section className='form'>
                 <FiUserPlus size="105" color="#17202a"/>
-                <h1>Texto</h1>
+                <h1>{alunoId === '0'? 'Incluir Novo Aluno' : 'Atualizar Aluno'}</h1>
                 <Link className="back-link" to="/alunos">
                     <FiCornerDownLeft size="25" color="#17202a"/>
                     Retornar
@@ -21,7 +26,7 @@ export default function NovoAluno(){
                     <input placeholder='Nome'/>
                     <input placeholder='Email'/>
                     <input placeholder='Idade'/>
-                    <button className="button" type="submit">Texto</button>
+                    <button className="button" type="submit">{alunoId === '0'? 'Incluir ' : 'Atualizar '}</button>
                 </form>
             </div>
         </div>
