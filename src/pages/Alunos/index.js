9eb929d6ffb1b50b0d2fb6c0aa67f4a9186b4dd3
chmov <chmov@PC-CARLOSHMV>
2026-04-19 18:18:09 -0300

    feat.: implementa edição, deleção e filtro - Alunos

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,6 +1,6 @@
 import './styles.css';
-import {Link, useNavigate } from 'react-router-dom';
-import React, {useState, useEffect} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 
 import { FiXCircle, FiEdit, FiUserX } from 'react-icons/fi';
@@ -9,86 +9,138 @@ import logoCadastro from '../../assets/cadastro1.png';
 
 export default function Alunos() {
 
-     //filtrar dados
-     const [searchInput,setSearchInput]  = useState('');
-     const [filtro, setFiltro] = useState([]);
+  //filtrar dados
+  const [searchInput, setSearchInput] = useState('');
+  const [filtro, setFiltro] = useState([]);
 
-     const [alunos, setAlunos] = useState([]);
+  const [alunos, setAlunos] = useState([]);
 
-     const email = localStorage.getItem('email');
-     const token = localStorage.getItem('token');
+  const email = localStorage.getItem('email');
+  const token = localStorage.getItem('token');
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const authorization = {
-         headers : {
-           Authorization : `Bearer ${token}`
-         }
-     }
+  const authorization = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
 
-    useEffect(() => {
-        if (!token) {
-            navigate('/');
-            return;}
+  useEffect(() => {
+    if (!token) {
+      navigate('/');
+      return;
+    }
 
-        api.get('api/alunos', authorization)
-       .then(response => {
+    api.get('api/alunos', authorization)
+      .then(response => {
         setAlunos(response.data);
-    })
-    
-    .catch(err => {
-      if (err.response?.status === 401) {logout();}
-      else { console.error(err);}
-    });}, [token]);
+      })
 
-    
-    async function logout(){
-       try{
-          localStorage.clear();
-          localStorage.setItem('token','');
-          authorization.headers ='';
-          navigate('/'); 
-       }catch(err){
-        alert('Não foi possível fazer o logout' + err);
-       }
-     }
-    
-    
-    return(
-        <div className='aluno-container'>
-            <header>
-                <img src={logoCadastro} alt="Cadastro" />
-                <span>Bem vindo, <strong>{email}</strong></span>
-                <Link className='button' to="/aluno/novo/0">Novo Aluno</Link>
-                <button onClick={logout} type='button'>
-                    <FiXCircle size={35} color='#17202a'></FiXCircle>
-                </button>
-            </header>
-            <form>
-                <input type='text' placeholder='Nome'></input>
-                <button type='button' className='button'>Filtra aluno por nome (parcial)</button>
-            </form>
-            <h1>Relação de Alunos</h1>
-            <ul>
-               {alunos.map(aluno=>(
-                 <li key={aluno.id}>
-                  <b>Nome:</b>{aluno.nome}<br/><br/>
-                  <b>Email:</b>{aluno.email}<br/><br/>
-                  <b>Idade:</b>{aluno.idade}<br/><br/>
-{/* 
-                <button onClick={()=> editAluno(aluno.id)} type="button">
-                     <FiEdit size="25" color="#17202a" />
-                 </button> */}
-
-                 {/* <button type="button" onClick= {()=> deleteAluno(aluno.id)}> 
-                     <FiUserX size="25" color="#17202a" />
-                 </button> */}
-               </li>
-               ))}
-            </ul>
-
-        </div>
+      .catch(err => {
+        if (err.response?.status === 401) { logout(); }
+        else { console.error(err); }
+      });
+  }, [token]);
 
 
-    )
+  async function logout() {
+    try {
+      localStorage.clear();
+      localStorage.setItem('token', '');
+      authorization.headers = '';
+      navigate('/');
+    } catch (err) {
+      alert('Não foi possível fazer o logout' + err);
+    }
+  }
+
+  async function editAluno(id) {
+    try {
+      navigate(`/aluno/novo/${id}`);
+    } catch (error) {
+      alert('Não foi possível editar o aluno')
+    }
+  }
+
+  async function deleteAluno(id) {
+    try {
+      if (window.confirm('Deseja deletar o aluno de id = ' + id + ' ?')) {
+        await api.delete(`api/alunos/${id}`, authorization);
+        setAlunos(alunos.filter(aluno => aluno.id !== id));
+      }
+    } catch (error) {
+      alert('Não foi possível excluir o aluno')
+    }
+  }
+
+  const searchAlunos = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== '') {
+      const dadosFiltrados = alunos.filter((item) => {
+        return Object.values(item).join('').toLowerCase()
+          .includes(searchInput.toLowerCase())
+      });
+      setFiltro(dadosFiltrados);
+    }
+    else {
+      setFiltro(alunos);
+    }
+  }
+
+
+  return (
+    <div className='aluno-container'>
+      <header>
+        <img src={logoCadastro} alt="Cadastro" />
+        <span>Bem vindo, <strong>{email}</strong></span>
+        <Link className='button' to="/aluno/novo/0">Novo Aluno</Link>
+        <button onClick={logout} type='button'>
+          <FiXCircle size={35} color='#17202a'></FiXCircle>
+        </button>
+      </header>
+      <form>
+        <input type='text'
+          placeholder='Filtrar por nome...'
+          onChange={(e) => searchAlunos(e.target.value)}
+        />
+      </form>
+      <h1>Relação de Alunos</h1>
+      {searchInput.length > 1 ? (
+        <ul>
+          {filtro.map(aluno => (
+            <li key={aluno.Id}>
+              <b>Nome:</b>{aluno.nome}<br /><br />
+              <b>Email:</b>{aluno.email}<br /><br />
+              <b>Idade:</b>{aluno.idade}<br /><br />
+              <button onClick={() => editAluno(aluno.id)} type="button">
+                <FiEdit size="25" color="#17202a" />
+              </button>
+              <button type="button" onClick={() => deleteAluno(aluno.id)}>
+                <FiUserX size="25" color="#17202a" />
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <ul>
+          {alunos.map(aluno => (
+            <li key={aluno.id}>
+              <b>Nome:</b>{aluno.nome}<br /><br />
+              <b>Email:</b>{aluno.email}<br /><br />
+              <b>Idade:</b>{aluno.idade}<br /><br />
+
+              <button onClick={() => editAluno(aluno.id)} type="button">
+                <FiEdit size="25" color="#17202a" />
+              </button>
+
+              <button type="button" onClick={() => deleteAluno(aluno.id)}>
+                <FiUserX size="25" color="#17202a" />
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
